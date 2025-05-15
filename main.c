siemens/efibootguard

commit 7c90e82abfbe53a0cec488bc1901c8582e0d3786

efi: implement systemd boot loader interface
This patch implements the LoaderDevicePartUUID part of the systemd boot
loader interface to pass data from the loader to the OS / systemd. The
data is passed via EFI variables which are set by the first-stage loader
(the one on the ESP), or alternatively by the first loader that is
executed. By that, userspace components can later inspect this variable
to e.g. limit the search for config partitions to the device it was
bootet from. Currently only the LoaderDevicePartUUID is implemented.

Technically, the loader asks the EFI API for the UUID of the partition it
is executed from. Normally that is the ESP partition. Then, this UUID is
assigned to the LoaderDevicePartUUID EFI variable (in case not set yet).
This logic is crucial to correctly support chain-loading uses-cases and
also aligned with how systemd boot implements this.

For the sake of completeness, this logic is also added to the efi stub.
When using it in combination with the EBG loader, this is irrelevant,
but when starting the UKI directly it is needed.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

main.c
@@ -22,6 +22,7 @@
 #include <configuration.h>
 #include "version.h"
 #include "utils.h"
+#include "loader_interface.h"
 
 extern const unsigned long init_array_start[];
 extern const unsigned long init_array_end[];
@@ -113,6 +114,7 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 	EFI_STATUS status;
 	BG_STATUS bg_status;
 	BG_LOADER_PARAMS bg_loader_params;
+	BG_INTERFACE_PARAMS bg_interface_params;
 	CHAR16 *tmp;
 
 	ZeroMem(&bg_loader_params, sizeof(bg_loader_params));
@@ -184,6 +186,16 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 		error_exit(L"Cannot load specified kernel image", status);
 	}
 
+	UINT16 *boot_medium_uuidstr =
+		disk_get_part_uuid(loaded_image->DeviceHandle);
+	bg_interface_params.loader_device_part_uuid = boot_medium_uuidstr;
+	status = set_bg_interface_vars(&bg_interface_params);
+	if (EFI_ERROR(status)) {
+		error_exit(L"Cannot set bootloader interface variables",
+			   status);
+	}
+	INFO(L"LoaderDevicePartUUID=%s\n", boot_medium_uuidstr);
+	FreePool(boot_medium_uuidstr);
 	FreePool(payload_dev_path);
 	FreePool(boot_medium_path);
 

kernel-stub/main.c
@@ -17,6 +17,7 @@
 
 #include "kernel-stub.h"
 #include "version.h"
+#include "loader_interface.h"
 
 typedef struct {
 	UINT8 Ignore[60];
@@ -113,6 +114,7 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 	const SECTION *section;
 	EFI_STATUS status, cleanup_status;
 	UINTN n, kernel_pages;
+	BG_INTERFACE_PARAMS bg_interface_params;
 
 	this_image = image_handle;
 	InitializeLib(image_handle, system_table);
@@ -229,6 +231,15 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 		}
 		info(L"Using firmware-provided device tree");
 	}
+
+	UINT16 *boot_medium_uuidstr =
+		disk_get_part_uuid(stub_image->DeviceHandle);
+	bg_interface_params.loader_device_part_uuid = boot_medium_uuidstr;
+	status = set_bg_interface_vars(&bg_interface_params);
+	if (EFI_ERROR(status)) {
+		error(L"could not set interface vars", status);
+	}
+	FreePool(boot_medium_uuidstr);
 
 	kernel_entry = (EFI_IMAGE_ENTRY_POINT)
 		((UINT8 *) kernel_image.ImageBase +

Makefile.am
@@ -185,12 +185,14 @@ efi_sources = \
 	env/syspart.c \
 	env/fatvars.c \
 	utils.c \
+	loader_interface.c \
 	bootguard.c \
 	main.c
 
 kernel_stub_name = kernel-stub$(MACHINE_TYPE_NAME).efi
 
 kernel_stub_sources = \
+	loader_interface.c \
 	kernel-stub/fdt.c \
 	kernel-stub/initrd.c \
 	kernel-stub/main.c

include/loader_interface.h
@@ -1,0 +1,25 @@
+/*
+ * EFI Boot Guard
+ *
+ * Copyright (c) Siemens AG, 2023
+ *
+ * Authors:
+ *  Felix Moessbauer <felix.moessbauer@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#pragma once
+
+typedef struct _BG_INTERFACE_PARAMS {
+	CHAR16 *loader_device_part_uuid;
+} BG_INTERFACE_PARAMS;
+
+// systemd bootloader interface vendor id
+extern EFI_GUID vendor_guid;
+
+EFI_STATUS set_bg_interface_vars(const BG_INTERFACE_PARAMS *params);
+CHAR16 *disk_get_part_uuid(EFI_HANDLE *handle);

loader_interface.c
@@ -1,0 +1,69 @@
+/*
+ * EFI Boot Guard
+ *
+ * Copyright (c) Siemens AG, 2023
+ *
+ * Authors:
+ *  Felix Moessbauer <felix.moessbauer@siemens.com>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2.  See
+ * the COPYING file in the top-level directory.
+ *
+ * SPDX-License-Identifier:	GPL-2.0
+ */
+
+#include <efi.h>
+#include <efilib.h>
+#include "loader_interface.h"
+#include "utils.h"
+
+EFI_GUID vendor_guid = {0x4a67b082,
+			0x0a4c,
+			0x41cf,
+			{0xb6, 0xc7, 0x44, 0x0b, 0x29, 0xbb, 0x8c, 0x4f}};
+
+EFI_STATUS set_bg_interface_vars(const BG_INTERFACE_PARAMS *params)
+{
+	EFI_STATUS status = EFI_SUCCESS;
+	UINT32 attribs =
+		EFI_VARIABLE_BOOTSERVICE_ACCESS | EFI_VARIABLE_RUNTIME_ACCESS;
+
+	// only set this if not set by a previous stage loader
+	UINTN readsize = 0;
+	if (RT->GetVariable(L"LoaderDevicePartUUID", &vendor_guid, NULL,
+			    &readsize, NULL) == EFI_NOT_FOUND) {
+		status = RT->SetVariable(
+			L"LoaderDevicePartUUID", &vendor_guid, attribs,
+			StrLen(params->loader_device_part_uuid) *
+				sizeof(UINT16),
+			params->loader_device_part_uuid);
+	}
+	return status;
+}
+
+CHAR16 *disk_get_part_uuid(EFI_HANDLE *handle)
+{
+	EFI_STATUS err;
+	EFI_DEVICE_PATH *dp;
+	err = BS->HandleProtocol(handle, &DevicePathProtocol, (void **)&dp);
+	if (EFI_ERROR(err)) {
+		return NULL;
+	}
+
+	for (; !IsDevicePathEnd(dp); dp = NextDevicePathNode(dp)) {
+		if (dp->Type != MEDIA_DEVICE_PATH ||
+		    dp->SubType != MEDIA_HARDDRIVE_DP) {
+			continue;
+		}
+
+		HARDDRIVE_DEVICE_PATH *hd = (HARDDRIVE_DEVICE_PATH *)dp;
+		if (hd->SignatureType != SIGNATURE_TYPE_GUID) {
+			continue;
+		}
+		UINT16 *buffer = AllocatePool(sizeof(UINT16) * 37);
+		GuidToString(buffer, (EFI_GUID *)hd->Signature);
+		return buffer;
+	}
+
+	return NULL;
+}

commit 474aca6588496343aaa1f7d8981b553b36d82614

kernel-stub: fix nullptr deref on error getting boot part UUID
The caller of disk_get_part_uuid is responsible for checking the return
value, as this function returns NULL in case of an error. This was not
done, leading to a nullptr dereference on error.

We fix this by checking the return code. Further, we now issue a error
message in case the UUID cannot be determined.

Fixes: 7c90e82 ("efi: implement systemd boot loader interface")
Reported-by: Jan Kiszka <jan.kiszka@siemens.com>
Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

main.c
@@ -203,14 +203,18 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 
 	UINT16 *boot_medium_uuidstr =
 		disk_get_part_uuid(loaded_image->DeviceHandle);
-	bg_interface_params.loader_device_part_uuid = boot_medium_uuidstr;
-	status = set_bg_interface_vars(&bg_interface_params);
-	if (EFI_ERROR(status)) {
-		WARNING(L"Cannot set bootloader interface variables (%r)\n",
-			status);
+	if (!boot_medium_uuidstr) {
+		WARNING(L"Cannot get boot partition UUID\n");
+	} else {
+		bg_interface_params.loader_device_part_uuid = boot_medium_uuidstr;
+		status = set_bg_interface_vars(&bg_interface_params);
+		if (EFI_ERROR(status)) {
+			WARNING(L"Cannot set bootloader interface variables (%r)\n",
+				status);
+		}
+		INFO(L"LoaderDevicePartUUID=%s\n", boot_medium_uuidstr);
+		FreePool(boot_medium_uuidstr);
 	}
-	INFO(L"LoaderDevicePartUUID=%s\n", boot_medium_uuidstr);
-	FreePool(boot_medium_uuidstr);
 	FreePool(payload_dev_path);
 	FreePool(boot_medium_path);
 