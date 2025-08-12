siemens/efibootguard

commit c2be7c1b95b4d5aa7debad8f4e41c3611cde4057

libebgenv: fix memory leak in partition probing
When probing partitions, the mountpoint string is allocated on the heap.
Normally the mountpoint is freed during unmounting. But for partitions
that are already mounted, we also need to free the mountpoint str.

Signed-off-by: Felix Moessbauer <felix.moessbauer@siemens.com>
[Jan: NULLify mountpoint to avoid double frees]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

env/env_config_file.c
@@ -85,6 +85,9 @@ bool probe_config_file(CONFIG_PART *cfgpart)
 		}
 		if (do_unmount) {
 			unmount_partition(cfgpart);
+		} else {
+			free(cfgpart->mountpoint);
+			cfgpart->mountpoint = NULL;
 		}
 		return result;
 	}

commit dc24d2d62c025b9bbefa0144c613db160ee503bd

libebgenv: Only release CONFIG_PART::mountpoint early on errors
This fixes a regression caused by premature cleanup: If
probe_config_file() ran successfully and found an already mounted
partition, its mountpoint must not be released yet. bgenv_finalize()
will take care of that.

However, if probe_config_file() failed, it must free any previously
created mountpoint string prior to returning. That is what c2be7c1b95b4
was actually trying to resolve.

But as we are no longer freeing in probe_config_file on success, we need
to free on errors in probe_config_partitions. This was found by coverity
after only changing probe_config_file.

Reported-by: Ingo Rah <ingo.rah@linutronix.de>
Fixes: c2be7c1b95b4 ("libebgenv: fix memory leak in partition probing")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

env/env_config_file.c
@@ -85,7 +85,7 @@ bool probe_config_file(CONFIG_PART *cfgpart)
 		}
 		if (do_unmount) {
 			unmount_partition(cfgpart);
-		} else {
+		} else if (!result) {
 			free(cfgpart->mountpoint);
 			cfgpart->mountpoint = NULL;
 		}

env/env_config_partitions.c
@@ -149,6 +149,7 @@ bool probe_config_partitions(CONFIG_PART *cfgpart, bool search_all_devices)
 					cfgpart[count] = tmp;
 				} else {
 					free(tmp.devpath);
+					free(tmp.mountpoint);
 					VERBOSE(stderr,
 						"Error, there are "
 						"more than %d config "