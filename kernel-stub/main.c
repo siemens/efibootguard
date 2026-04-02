siemens/efibootguard

commit 4b76201933bf11741789a6fe3c034cdea0116039

kernel-stub: Check for overflow when casting to VOID *
When building for 32 bit architectures, casting from
64 bit EFI_PHYSICAL_ADDRESS to 32 bit VOID * elicits a
compiler warning (-Wint-to-pointer-cast).

Check that the cast will not drop high order bits by using
uintptr_t as an intermediate type to transform the address
quietly.

Signed-off-by: Earl Chew <earl_chew@yahoo.com>
[Jan: fix comparison]
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kernel-stub/main.c
@@ -104,6 +104,7 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 	BOOLEAN has_dtbs = FALSE;
 	const VOID *kernel_source;
 	EFI_PHYSICAL_ADDRESS kernel_buffer;
+	EFI_PHYSICAL_ADDRESS aligned_kernel_buffer;
 	const CHAR8 *fdt_compatible;
 	VOID *fdt, *alt_fdt = NULL;
 	EFI_IMAGE_ENTRY_POINT kernel_entry;
@@ -193,8 +194,14 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 		goto cleanup_initrd;
 	}
 
-	kernel_image.ImageBase = (VOID *)
+	aligned_kernel_buffer =
 		align_addr(kernel_buffer, pe_header->Opt.SectionAlignment);
+	if ((uintptr_t) aligned_kernel_buffer != aligned_kernel_buffer) {
+		error(L"Alignment overflow for kernel image", EFI_LOAD_ERROR);
+		goto cleanup_buffer;
+	}
+
+	kernel_image.ImageBase = (VOID *) (uintptr_t) aligned_kernel_buffer;
 	kernel_image.ImageSize = kernel_section->VirtualSize;
 
 	CopyMem(kernel_image.ImageBase, kernel_source, kernel_image.ImageSize);

commit b41fc2a29274333d50a82768a33f34ca0370d15e

kernel-stub: Set error code alignment overflow
Avoid reporting an error but then not returning a proper error code to
the firmware. Impact is rather cosmetic, latest the watchdog will catch
this early termination.

Fixes: 4b76201933bf ("kernel-stub: Check for overflow when casting to VOID *")
Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

kernel-stub/main.c
@@ -200,6 +200,7 @@ EFI_STATUS efi_main(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE *system_table)
 		align_addr(kernel_buffer, pe_header->Opt.SectionAlignment);
 	if ((uintptr_t) aligned_kernel_buffer != aligned_kernel_buffer) {
 		error(L"Alignment overflow for kernel image", EFI_LOAD_ERROR);
+		status = EFI_LOAD_ERROR;
 		goto cleanup_buffer;
 	}
 